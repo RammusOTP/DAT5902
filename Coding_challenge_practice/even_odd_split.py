79086a55065739477abf8c8cb28f9c590479551b
RammusOTP <emmanuelphillips33@gmail.com>
2024-10-14 15:25:27 +0100

    Function that splits a list of numbers into a list for even and list for odd numbers

---
+++ Coding_challenge_practice/even_odd_split.py
@@ -0,0 +1,15 @@
+def even_odd_split(numbers):
+    l_even = []
+    l_odd = []
+    for num in numbers:
+        if num % 2 == 0:
+            l_even.append(num)
+        else:
+            l_odd.append(num)
+    return l_even, l_odd
+
+
+numbers = [1, 2, 3, 4, 5, 6]
+even, odd = even_odd_split(numbers)
+print(even)
+print(odd)
