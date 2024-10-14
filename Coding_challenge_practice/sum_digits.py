23b1ee03ea6cec9a252f70e3b64733de3ed46aba
RammusOTP <emmanuelphillips33@gmail.com>
2024-10-14 22:38:13 +0100

    Function that sums digits of an integer

--- Coding_challenge_practice/sum_digits.py
+++ Coding_challenge_practice/sum_digits.py
@@ -4,4 +4,11 @@ def sum_digits(n):
     nlist = [i for i in str(n)]
     return sum(int(j) for j in nlist)
 
-print(sum_digits(int(input("Enter a number"))))
+#print(sum_digits(int(input("Enter a number: "))))
+
+"""Alternative solution from ChatGPT"""
+
+def sum_digits(n):
+    return sum(int(i) for i in str(abs(n))) # Forgot abs in my solution to deal with negative numbers
+
+print(sum_digits(int(input("Enter a number: "))))
