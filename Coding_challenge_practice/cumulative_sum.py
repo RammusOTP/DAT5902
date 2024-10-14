bc76b2a3c2a490d7811b2333009ea164a4231036
RammusOTP <emmanuelphillips33@gmail.com>
2024-10-14 15:36:41 +0100

    Function that returns a list of the cumulative sum of a list

---
+++ Coding_challenge_practice/cumulative_sum.py
@@ -0,0 +1,11 @@
+def cumulative_sum(numbers):
+    cumulative_list = [numbers[0]]
+    curr = numbers[0]
+    for i in range(1, len(numbers)):
+        curr += numbers[i]
+        cumulative_list.append(curr)
+    return cumulative_list
+        
+
+numbers =  [1, 2, 3, 4]
+print(cumulative_sum(numbers))
